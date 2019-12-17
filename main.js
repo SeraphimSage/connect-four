7d91a4385e3883b84bba07ceeb92502698eeccde
Jackson Nathaniel Detke <jndetke2150@gmail.com>
2019-12-17 14:56:06 -0500

    Commit planning document

---
+++ main.js
@@ -0,0 +1,22 @@
+// Group Members: Jack Detke, Kenneth Pinkerton, Dean Camacho
+// Kenzie Academy—- Software Engineering Majors, October 2019 Cohort Q1
+// Outline created December 17, 2019
+// Connect Four Assessment Outline
+
+/* 1.	Display game area and create a grid/ dataset for the game area. 
+    a.	Create a 7 column-by-6 row game area with a div consisting of various smaller row, column, 
+    diagonal, and cell divs (kind of like in the maze assessment).
+    b.	Display grid to show separation of game area by using border styling in CSS.
+    c.	Create a dataset to assign values to each cell so they’ll be easier to send each chip to.
+2.	Chips alternate colors for players and drop into chosen columns.
+    a.	Create either a click handler or a keyboard event listener to assign columns input so players 
+    can choose where chips will appear (kind of like in the tower of hanoi).
+    b.	Create check to see which player is going next and assign a chip class with the red color to one 
+    player and the black color to the other to form opposing players.
+    c.	Add animation for chips.
+    d.	Add a conditional statement to detect collisions into cells with chips already in them to make 
+    sure that chips fall on top of each other, not underneath each other.
+3.	Game checks vertical, horizontal, and diagonal to see if game is won.
+    a.	Create loop to search for win condition. 
+    b.	Display alert when conditions are met.
+    c.	Restart the game (and maybe keep score?) */
