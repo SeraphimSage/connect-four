a7612bd7708d7f7f304dacc0ce170ec498e03a26
Jackson Nathaniel Detke <jndetke2150@gmail.com>
2019-12-18 22:06:37 -0500

    Revision number one.

--- main.js
+++ main.js
@@ -3,7 +3,8 @@
 // Outline created December 17, 2019
 // Connect Four Assessment Outline
 
-/* 1.	Display game area and create a grid/ dataset for the game area. 
+/* 
+1.	Display game area and create a grid/ dataset for the game area. 
     a.	Create a 7 column-by-6 row game area with a div consisting of various smaller row, column, 
     diagonal, and cell divs (kind of like in the maze assessment).
     b.	Display grid to show separation of game area by using border styling in CSS.
@@ -19,4 +20,119 @@
 3.	Game checks vertical, horizontal, and diagonal to see if game is won.
     a.	Create loop to search for win condition. 
     b.	Display alert when conditions are met.
-    c.	Restart the game (and maybe keep score?) */
+    c.	Restart the game (and maybe keep score?) 
+*/
+
+// map of the connect four board in the form of nested arrays as rows containing all of the cells
+let boardModel = [
+    ['', '', '', '', '', ''],
+    ['', '', '', '', '', ''],
+    ['', '', '', '', '', ''],
+    ['', '', '', '', '', ''],
+    ['', '', '', '', '', ''],
+    ['', '', '', '', '', ''],
+    ['', '', '', '', '', '']
+]
+// selects the connect four board div container as the location for which the board will show up
+var board = document.getElementById("board");
+
+const initializeGame = function () {
+    const displayBoard;
+    const displayInstructions();
+    const displayActivePlayer();
+    const setUpColumnClickHandlers();
+    }
+initializeGame;
+
+const setUpColumnClickHandlers = function () {
+    const displayMessage = function (message) {
+        // showing the message in the DOM in the msgbox
+    }
+    const displayInstructions = function () {
+        displayMessage("Here's how to play...")
+    }
+    const displayWinningMsg = function (winningPlayer) {
+        displayMessage(`${winningPlayer} has won!`)
+    }
+    const displayTieMsg = function () {
+        displayMessage("Tie game")
+    }
+// creates player character global variables (still need to assign them to a CSS class)
+// var redPlayer;
+// var blackPlayer;
+var currentPlayer = "red";
+
+// creates empty row (and cell) variables
+var column; 
+var columnElement;
+var cell;
+var cellElement;
+
+// creates a for loop that adds each nested array in the boardModel variable (row on the connect four board)
+// by storing it in the rowElemennt variable
+for (let columnIndex = 0; columnIndex <= 6; columnIndex++) {
+    column = boardModel[columnIndex];
+
+    // creates a div to eventually be placed within the board div for each individual row
+    columnElement = document.createElement("div");
+
+    // styles the rows in ways that apply to every row by giving them a class name 
+    // that's connected to the CSS stylesheet
+    columnElement.classList.add("column");
+
+    // appends every row to the page via div within the board div
+    board.appendChild(columnElement);
+
+    // nested for loop adds each individual cell in each individual row to the connect four board by storing
+    // them in the cellElement variable
+    for (let cellIndex = 0; cellIndex <= 5; cellIndex++) {
+        cell = column[cellIndex];
+
+        // creates a div for each individual cell to eventually be placed within each row div
+        cellElement = document.createElement("div");
+
+        // a dataset that assigns each cell a column and row value
+        cellElement.dataset.columnNumber = columnIndex;
+        cellElement.dataset.rowNumber = cellIndex;
+
+        // styles the cells in ways that apply to every cell by giving them a class name 
+        // that's connected to the CSS stylesheet
+        cellElement.classList = "cell";
+
+        // appends every cell to the page via div within each row div
+        columnElement.appendChild(cellElement);
+
+        const displayActivePlayer = function () {}
+        const determineGameWinner = function (board) {
+            return '' // TODO: implement for real
+        }
+
+        columnElement.addEventListener("click", playerMove);
+        function playerMove(event) {
+            let currentColumnElement = event.target;
+            let bottomCell = currentColumnElement.lastElementChild;
+            let chipElement = document.createElement("div");
+            chipElement.classList.add("chips");
+            bottomCell.appendChild(chipElement);
+            if (determineGameWinner(boardModel) !== '') {
+                displayWinningMsg()
+            } else if (gameIsATie(boardModel[column])) {
+                displayTieMsg()
+            } else {
+                switchToNextPlayer()
+            }
+        };
+    }
+    playerMove(currentColumnElement);
+};
+    // creates 
+    // listens to keyboard events
+    /* columnElement.addEventListener("click", playerMove);
+    
+    function playerMove() {
+        let currentColumnElement = event.currentTarget;
+        let bottomCell = currentColumnElement.lastElementChild;
+        let chipElement = document.createElement("div");
+        
+        if
+    }; */
